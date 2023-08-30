8aaa69ea83e94c4ba9535c071335280283fb6f10
Al1erEgo <al1erego@gmail.com>
2023-08-30 16:59:12 +0200

    feat: add react navigation and expo router

--- app/App.tsx
+++ app/App.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { FilmsList } from '../modules/films-list/screens/films-list';
 import NavigationContainer from 'expo-router/build/fork/NavigationContainer.native';
+import 'react-native-gesture-handler';
 
 export function App(): ReactElement {
   return (
