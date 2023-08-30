f59d8368184a1e59d731bc22b9f66a07607fbe71
Al1erEgo <al1erego@gmail.com>
2023-08-30 12:23:26 +0200

    feat: add additional eslint plugins and config

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,14 @@
 import { ReactElement } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { FilmsList } from '../modules/films-list/screens/films-list';
+import NavigationContainer from 'expo-router/build/fork/NavigationContainer.native';
 
 export function App(): ReactElement {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <FilmsList />
+      <NavigationContainer>
+        <FilmsList />
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
