0572f07be778a949ef467dac05cfed8da3c5c95b
Al1erEgo <al1erego@gmail.com>
2023-09-01 10:47:58 +0200

    fix: eslint rules st2

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,7 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { ReactElement } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { FilmsList } from '../modules/films-list/screens/films-list';
-import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
 import { FilmDetails } from '../modules/films-list/screens/film-details';
@@ -13,9 +13,9 @@ export function App(): ReactElement {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="FilmsList">
-          <Stack.Screen name="FilmsList" component={FilmsList} />
-          <Stack.Screen name="FilmDetails" component={FilmDetails} />
+        <Stack.Navigator initialRouteName='FilmsList'>
+          <Stack.Screen name='FilmsList' component={FilmsList} />
+          <Stack.Screen name='FilmDetails' component={FilmDetails} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
